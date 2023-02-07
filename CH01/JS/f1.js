d7cca9ea0a20bd442aca13c1b6fda67d3f5494dc
Summer Shields <77825540+summerjusticeshields@users.noreply.github.com>
2023-02-07 10:16:40 -0800

    Add files via upload

---
+++ CH01/JS/f1.js
@@ -0,0 +1,19 @@
+// This will output to the console
+
+console.log("Hello from Yoruba Android");
+
+console.log("Hello from JavaScript!");
+console.log("Let's do some math");
+// This is a single line comment
+console.log(4 + 7);
+console.log(8 - 2);
+console.log(2 * 5);
+console.log(12 / 4);
+console.log(12 / 0);
+//Play with Variable section of JSWay Book
+const name = prompt("Enter your name:")
+alert
+
+const input = prompt
+const nb = Number(input);
+
